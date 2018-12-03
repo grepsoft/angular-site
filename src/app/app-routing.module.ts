d89a68db63597a181e3cf86d795731c9324262f6
rsiraj <r.siraj.r@gmail.com>
2018-12-03 13:51:49 -0500

    fixed routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-
-const routes: Routes = [];
+import { ProjectsComponent } from './projects/projects.component';
+import { HomeComponent } from './home/home.component';
+import { ContactusComponent } from './contactus/contactus.component'
+const routes: Routes = [
+      { path: 'home', component: HomeComponent},
+      { path: "projects", component: ProjectsComponent },
+      { path: 'contactus', component: ContactusComponent },
+      { path: '', component: HomeComponent},
+      { path: '**', component: HomeComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
